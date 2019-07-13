5be5d48726c600d30360e10caae9eb6a2f2eca22
yang <yangxl1011@163.com>
2019-07-13 16:09:36 +0800

    更新

--- BaiXian/src/main/java/com/zl/user/controller/CompanyInfoController.java
+++ BaiXian/src/main/java/com/zl/user/controller/CompanyInfoController.java
@@ -1,14 +1,22 @@
 package com.zl.user.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.zl.pojo.user.CompanyInfo;
 import com.zl.user.service.CompanyInfoService;
 
-
+/**
+ * 添加企业认证信息
+ *   -->控制层
+ */
 
 @Controller
 @RequestMapping("/companyInfo")
@@ -16,28 +24,63 @@ public class CompanyInfoController {
 	@Autowired
 	private CompanyInfoService cs;
 	
-	/**
-	 * 添加企业认证信息
-	 *   -->返回结果
-	 */
 	//转到添加页面
 	@RequestMapping("/toAddCompanyInfo")
+	@ResponseBody
 	public ModelAndView toAddCompanyInfo() {
-		System.out.println("进入跳转添加页面控制器.................");
+		System.out.println("进入跳转企业添加页面控制器.................");
 		ModelAndView mv=new ModelAndView();
-		mv.setViewName("addCompanyInfo");
+		mv.setViewName("");
 		return mv;
 	}
 	
 	//添加
 	@RequestMapping("/addCompanyInfo")
-	public String addCompanyInfo(CompanyInfo companyInfo) {
-		int flag;
-		System.out.println("进入添加控制器.................");
-		flag = cs.addCompanyInfo(companyInfo);
-		System.out.println(flag);
-		return "redirect:/new"; 
+	@ResponseBody
+	public Map<String,Object> addCompanyInfo(CompanyInfo companyInfo) {
+		System.out.println("进入企业添加控制器.................");
+		Map<String,Object> map = new HashMap<>();
+		cs.addCompanyInfo(companyInfo);
+		map.put("true", 1); //状态码
+		map.put("cg", "成功"); //结果
+		return map; 
 	}
+	
+	//根据id查询
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	@ResponseBody
+	public Map<String,Object> findCompanyInfoById(int id){
+		System.out.println("进入企业id查询控制器..................");
+		Map<String,Object> map = new HashMap<>();
+		CompanyInfo companyInfo = cs.findCompanyInfoById(id);
+		map.put("true", 1);//状态码
+		map.put("cg", "成功"); //结果
+		map.put("data",companyInfo);
+		return map;
+	}
+	
+	//跳转更新页面
+	@RequestMapping("/toUpdateCompanyInfo")
+	@ResponseBody
+	public ModelAndView toUpdateCompanyInfo() {
+		System.out.println("进入跳转企业更新页面控制器.................");
+		ModelAndView mv=new ModelAndView();
+		mv.setViewName("");
+		return mv;
+	}
+	
+	//更新
+	@RequestMapping("/updateCompanyInfo")
+	@ResponseBody
+	public Map<String,Object> updateCompanyInfo(CompanyInfo companyInfo) {
+		System.out.println("进入企业更新页面控制器.................");
+		Map<String,Object> map = new HashMap<>();
+		cs.updateCompanyInfo(companyInfo);
+		map.put("true", 1);//状态码
+		map.put("cg", "成功");  //结果
+		return map;
+	}
+	
 	
 	
 	
--- BaiXian/src/main/java/com/zl/user/controller/ConfirmController.java
+++ BaiXian/src/main/java/com/zl/user/controller/ConfirmController.java
@@ -1,13 +1,22 @@
 package com.zl.user.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.zl.pojo.user.CompanyInfo;
 import com.zl.pojo.user.Confirm;
 import com.zl.user.service.ConfirmService;
+
+/**
+ * 个人认证信息
+ * 	-->控制层
+ */
 
 @Controller
 @RequestMapping("/confirm")
@@ -15,14 +24,11 @@ public class ConfirmController {
 	@Autowired
 	private ConfirmService cs;
 	
-	/**
-	 * 添加个人认证信息
-	 *   -->返回结果
-	 */
 	//转到添加页面
 	@RequestMapping("/toAddConfirm")
+	@ResponseBody
 	public ModelAndView toAddConfirm() {
-		System.out.println("进入跳转添加页面控制器.................");
+		System.out.println("进入跳转个人添加页面控制器.................");
 		ModelAndView mv=new ModelAndView();
 		mv.setViewName("addConfirm");
 		return mv;
@@ -30,11 +36,48 @@ public class ConfirmController {
 		
 	//添加
 	@RequestMapping("/addConfirm")
-	public String addConfirm(Confirm confirm) {
-		int flag;
-		System.out.println("进入添加控制器.................");
-		flag = cs.addConfirm(confirm);
-		System.out.println(flag);
-		return "redirect:/new"; 
+	@ResponseBody
+	public Map<String,Object> addConfirm(Confirm confirm) {
+		System.out.println("进入个人添加控制器.................");
+		Map<String,Object> map = new HashMap<>();
+		cs.addConfirm(confirm);
+		map.put("true", 1); //状态码
+		map.put("cg", "成功"); //结果
+		return map; 
 	} 
+	
+	//根据id查询
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	@ResponseBody
+	public Map<String,Object> findConfirmById(int id){
+		System.out.println("进入个人id查询控制器.................");
+		Map<String,Object> map = new HashMap<>();
+		Confirm confirm = cs.findConfirmById(id);
+		map.put("true", 1);//状态码
+		map.put("cg", "成功"); //结果
+		map.put("data",confirm);
+		return map;
+	}
+	
+	//跳转更新页面
+	@RequestMapping("/toUpdateConfirm")
+	@ResponseBody
+	public ModelAndView toUpdateConfirm() {
+		System.out.println("进入跳转个人更新页面控制器.................");
+		ModelAndView mv=new ModelAndView();
+		mv.setViewName("");
+		return mv;
+	}
+		
+	//更新
+	@RequestMapping("/updateConfirm")
+	@ResponseBody
+	public Map<String,Object> updateConfirm(Confirm confirm) {
+		System.out.println("进入个人更新页面控制器.................");
+		Map<String,Object> map = new HashMap<>();
+		cs.updateConfirm(confirm);
+		map.put("true", 1);//状态码
+		map.put("cg", "成功");  //结果
+		return map;
+	}
 }
