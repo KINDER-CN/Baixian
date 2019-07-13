3eaf20cc3271fe15f858f997a8490a71cd7ce912
Luoxianfeng <Luoxianfeng @TQS2Z6YLXLB6YHV>
2019-07-13 10:46:42 +0800

    资源单dao层

---
+++ BaiXian/src/main/java/com/zl/dao/resource/ClassThreeDao.java
@@ -0,0 +1,11 @@
+package com.zl.dao.resource;
+
+import com.zl.pojo.resource.ClassThree;
+
+public interface ClassThreeDao {
+	
+	/**
+	 * 资源物品单ResourcesItems级联查询三级分类表class_three
+	*/
+	public ClassThree findClassThreeById();
+}
---
+++ BaiXian/src/main/java/com/zl/dao/resource/ResourcesSingleDao.java
@@ -0,0 +1,11 @@
+package com.zl.dao.resource;
+
+import com.zl.pojo.resource.ResourcesSingle;
+
+public interface ResourcesSingleDao {
+	
+	/**
+	 * 查询资源单ResourcesSingle
+	*/
+	public ResourcesSingle findResourcesSingleById();
+}
