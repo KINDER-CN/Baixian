6d306988139e712f57cee979ef3d3eaa36fef742
Kinder <kinder@163.com>
2019-07-18 14:27:06 +0800

    资源物品添加

--- BaiXian/src/main/java/com/zl/resouce/controller/ResourcesItemsConverter.java
+++ BaiXian/src/main/java/com/zl/resouce/controller/ResourcesItemsConverter.java
@@ -1,6 +1,7 @@
 package com.zl.resouce.controller;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -8,8 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.zl.pojo.Paging;
 import com.zl.pojo.resource.ResourcesItems;
 import com.zl.pojo.resource.ResourcesSingle;
@@ -19,12 +24,26 @@ import com.zl.resouce.service.ResourcesItemsService;
 public class ResourcesItemsConverter {
 	@Autowired
 	private ResourcesItemsService ris;
-	@RequestMapping("/add")
-	public String toAddLib(Map<String,Object> map) {
-		//List<ResourcesItems> res=ris.queryLibt();
-		//map.put("res", res);
-		return "addLib";
-	}
+	/*
+	 * 资源物品添加
+	 * */
+	@ResponseBody
+	@RequestMapping("/test")
+		 public String testUser(@RequestParam(value = "resources") String data) {
+	        JsonArray ja = new Gson().fromJson(data, JsonArray.class);
+	        Iterator<JsonElement> ija = ja.iterator();
+	        int i = 1;
+	        while (ija.hasNext()) {
+	        	ResourcesItems resource = new Gson().fromJson(ija.next(), ResourcesItems.class);
+	            System.out.println("第" + i + "条资源的名称" + resource.toString());
+	            i++;
+	            int aa = (int)(Math.random() * 1000000);
+	            resource.setrNo(aa);
+	            resource.setBrand("福喜");
+	            ris.addResourcesItems(resource);
+	        }
+	        return data;
+	    }
 	@RequestMapping(method=RequestMethod.POST)
 	public String addLib(ResourcesItems resourcesItems,ResourcesSingle resourcesSingle) throws Exception {
 		ris.addResourcesItems(resourcesItems);
